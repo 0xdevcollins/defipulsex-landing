4a9c6ea13c647c4696ca8131b16a1063660aec49
Collins Ikechukwu <collinschristroa@gmail.com>
2025-02-13 22:45:12 +0100

    feat(home): fetch and display plans data from dashboard API in home view

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,10 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class HomeController extends Controller
 {
     public function index () {
-        return view('home');
+        $response = Http::get(dashboard_url('/plans'));
+
+        if ($response->successful()) {
+            $plans = $response->json();
+        } else {
+            $plans = [];
+        }
+        return view('home', [
+            'plans' => $plans['data'],
+        ]);
     }
 }
