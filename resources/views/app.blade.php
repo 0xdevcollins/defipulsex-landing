162307a985e4d4eb4db3c94721e461508c424c9e
Collins Ikechukwu <collinschristroa@gmail.com>
2025-02-13 18:40:09 +0100

    feat: add Tawk.to live chat script to app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -19,6 +19,21 @@
     <link rel="stylesheet" href="assets/css/nice-select.css">
     <link rel="stylesheet" href="assets/css/main.css">
 
+    <!--Start of Tawk.to Script-->
+    <script type="text/javascript">
+        var Tawk_API = Tawk_API || {},
+            Tawk_LoadStart = new Date();
+        (function() {
+            var s1 = document.createElement("script"),
+                s0 = document.getElementsByTagName("script")[0];
+            s1.async = true;
+            s1.src = 'https://embed.tawk.to/67ae2d5c825083258e14c128/1ik062cl5';
+            s1.charset = 'UTF-8';
+            s1.setAttribute('crossorigin', '*');
+            s0.parentNode.insertBefore(s1, s0);
+        })();
+    </script>
+    <!--End of Tawk.to Script-->
 </head>
 
 <body>
